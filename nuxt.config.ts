c927978f5cdc5e353e95a0df5ca51f386dbde54d
Rihan <me@file.properties>
2024-01-10 22:37:09 +0000

    chore(deps): :arrow_up: upgrade dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "@nuxthq/ui",
+    "@nuxt/ui",
     "@nuxtjs/google-fonts",
     "@nuxtjs/fontaine",
     "@vueuse/nuxt",
